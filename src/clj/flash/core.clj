7ae54ee1e3151bcac9dea39539ec4c848ecbc2ba
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2021-06-02 11:42:57 +0300

    Initial commit

---
+++ src/clj/flash/core.clj
@@ -0,0 +1,36 @@
+(ns flash.core
+  (:require [ring.util.response :refer [response content-type]]
+            [compojure.core :refer [routes GET]]))
+
+(defn flash-message
+  "Flash handler"
+  [{session :session :as request}]
+  (if-let [message (:message session)]
+    (let [session (dissoc session :message)]
+      (-> (pr-str message)
+          response
+          (assoc :session session)
+          (content-type "application/edn")))
+    (-> (pr-str {:message nil})
+        response
+        (content-type "application/edn"))))
+
+(defn flash
+  "Flash route"
+  [_]
+  (routes
+   (GET "/flash" [] flash-message)))
+
+(defn wrap-message
+  "Flash middleware"
+  [handler]
+  (fn [{flash :flash :as request}]
+    (if flash
+      (let [resp (handler request)
+            session (if (:session resp)
+                      (-> (:session resp)
+                          (assoc :message flash))
+                      (-> (:session request)
+                          (assoc :message flash)))]
+        (assoc resp :session session))
+      (handler request))))
---
+++ src/cljs/flash/om.cljs
@@ -0,0 +1,52 @@
+(ns flash.om
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.reader :refer [read-string]]))
+
+(defn warn [cursor message]
+  (om/update! (cursor) {:message message :level :warning :timestamp (.getTime (js/Date.))}))
+
+(defn bless [cursor message]
+  (om/update! (cursor) {:message message :level :success :timestamp (.getTime (js/Date.))}))
+
+(defn alert [cursor message]
+  (om/update! (cursor) {:message message :level :danger :timestamp (.getTime (js/Date.))}))
+
+(defn info [cursor message]
+  (om/update! (cursor) {:message message :level :info :timestamp (.getTime (js/Date.))}))
+
+(defn display [flash payload]
+  (when-let [message (:message payload)]
+    (case (:level payload)
+      :success (bless flash message)
+      :warning (warn flash message)
+      :info (info flash message)
+      :danger (alert flash message)
+      (info flash message))))
+
+(defn widget [{:keys [timeout flash]} owner]
+  (reify
+    om/IDisplayName
+    (display-name [this]
+      "flash")
+    om/IWillReceiveProps
+    (will-receive-props [this next-props]
+      (when-not (empty? (flash))
+        (js/clearTimeout (om/get-state owner :handler-id))
+        (om/set-state! owner :handler-id (js/setTimeout #(om/update! (flash) {}) (* timeout 1000)))))
+    om/IRender
+    (render [_]
+      (let [flash (om/observe owner (flash))
+            types {:success {:class "alert-success" :prefix "Well done!"} 
+                   :info {:class "alert-info" :prefix "Info!"}
+                   :warning {:class "alert-warning" :prefix "Sorry!"}
+                   :danger {:class "alert-danger" :prefix "Oh no!"}}]
+        (when-not (empty? flash)
+          (dom/div #js {:id "flash"
+                        :className (str "alert fade in " (:class ((:level flash) types)))}
+            (dom/strong nil (:prefix ((:level flash) types)))
+            (str " " (:message flash))))))))
+
+(defn get-messages [flash]
+  (.send goog.net.XhrIo "/flash" (fn [e] (let [payload (.getResponseText (.-target e))]
+                                          (display flash (read-string payload))))))
