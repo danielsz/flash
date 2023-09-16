22258ce23f2938f4d7351f841406b4b28216ce4c
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2023-09-16 17:19:37 +0300

    Flash overhaul

--- src/clj/flash/core.clj
+++ src/clj/flash/core.clj
@@ -1,36 +1,14 @@
 (ns flash.core
-  (:require [ring.util.response :refer [response content-type]]
-            [compojure.core :refer [routes GET]]))
+  (:require [ring.util.response :refer [response content-type]]))
 
-(defn flash-message
-  "Flash handler"
-  [{session :session :as request}]
-  (if-let [message (:message session)]
-    (let [session (dissoc session :message)]
-      (-> (pr-str message)
-          response
-          (assoc :session session)
-          (content-type "application/edn")))
-    (-> (pr-str {:message nil})
-        response
-        (content-type "application/edn"))))
-
-(defn flash
-  "Flash route"
-  [_]
-  (routes
-   (GET "/flash" [] flash-message)))
-
-(defn wrap-message
-  "Flash middleware"
-  [handler]
-  (fn [{flash :flash :as request}]
-    (if flash
-      (let [resp (handler request)
-            session (if (:session resp)
-                      (-> (:session resp)
-                          (assoc :message flash))
-                      (-> (:session request)
-                          (assoc :message flash)))]
-        (assoc resp :session session))
-      (handler request))))
+(def flash-route
+  ["/flash" {:get (fn [{session :session :as request}]
+                    (if-let [flash (:flash session)]                      
+                      (let [session (dissoc session :flash)]
+                        (-> (pr-str flash)
+                           response
+                           (assoc :session session)
+                           (content-type "application/edn")))
+                      (-> ""
+                         response
+                         (content-type "text/html"))))}])
--- src/cljs/flash/react.cljs
+++ src/cljs/flash/react.cljs
@@ -5,39 +5,24 @@
   (:require-macros [cljs-utils.compilers.hicada :refer [html]])
   (:import [goog.net XhrIo]))
 
-(defn warn [app-state message]
-  (swap! app-state assoc :flash {:message message :level :warning :timestamp (.getTime (js/Date.))}))
-(defn bless [app-state message]
-  (swap! app-state assoc :flash {:message message :level :success :timestamp (.getTime (js/Date.))}))
-(defn alert [app-state message]
-  (swap! app-state assoc :flash {:message message :level :danger :timestamp (.getTime (js/Date.))}))
-(defn info [app-state message]
-  (swap! app-state assoc :flash {:message message :level :info :timestamp (.getTime (js/Date.))}))
+(defonce state (atom {}))
 
-(defn display [app-state payload]
-  (when-let [message (:message payload)]
-    (case (:level payload)
-      :success (bless app-state message)
-      :warning (warn app-state message)
-      :info (info app-state message)
-      :danger (alert app-state message)
-      (info app-state message))))
-
-(defn widget [props]  
-  (let [[timeout app-state fetch] ((juxt :timeout :state :fetch) (js->clj props :keywordize-keys true))
-        get-messages (fn [app-state]
-                       (.send XhrIo "/flash" (fn [e] (let [payload (.getResponseText (.-target e))]
-                                                               (display app-state (read-string payload))))))
-        flash (useLens app-state :flash)
+(defn widget [props]
+  (let [{timeout :timeout } (js->clj props :keywordize-keys true)
+        flash (useLens state :flash)
         types {:success {:class "alert-success" :prefix "Well done!"} 
                :info {:class "alert-info" :prefix "Info!"}
                :warning {:class "alert-warning" :prefix "Sorry!"}
                :danger {:class "alert-danger" :prefix "Oh no!"}}]
-    (react/useEffect (fn [] (get-messages app-state)) #js [])
-    (react/useEffect (fn [] (js/setTimeout #(swap! app-state assoc :flash {}) (* timeout 1000))) #js [flash])
+    (react/useEffect (fn [] (let [id (js/setTimeout #(swap! state assoc :flash {}) (* timeout 1000))]
+                             (fn [] (js/clearTimeout id)))) #js [flash])
     (when (seq flash)
       (html [:div {:id "flash"
                    :className (str "alert fade in " (:class ((:level flash) types)))}
              [:strong (:prefix ((:level flash) types))]
              (str " " (:message flash))]))))
 
+(defn check []
+  (.send XhrIo "/flash" (fn [e] (let [payload (read-string (.getResponseText (.-target e)))]                                 
+                                 (when (seq payload)
+                                   (swap! state assoc :flash payload))))))
